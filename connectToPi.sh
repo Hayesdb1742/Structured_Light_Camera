d009b60700c99ffbbd15e9a5d3053dcbca06c3c3
Hayesdb1742 <hayesdbentley1@live.com>
2023-09-25 13:42:48 -0400

    Updated plink connection
    Actually run the script on Pi

--- connectToPi.sh
+++ connectToPi.sh
@@ -5,10 +5,10 @@ PI_USERNAME="lightwork"
 PI_HOST="raspberrypi.local"
 PI_PASSWORD="TEAM10"
 
-export DISPLAY=:0.0
+export DISPLAY=:0
 
 # Path to the shell script to execute on the Raspberry Pi
 REMOTE_SCRIPT="home/lightwork/scripts/imageControl.sh"
 
 # Execute the SSH connection and script execution
-plink -ssh -X $PI_USERNAME@$PI_HOST -pw $PI_PASSWORD
+plink -ssh -X $PI_USERNAME@$PI_HOST -pw $PI_PASSWORD -m $REMOTE_SCRIPT
