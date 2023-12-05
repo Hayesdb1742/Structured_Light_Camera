b4a0fcc0fef0009fd83ba7afe9e9139b6f77ac06
LudBot0 <nludwig42@gmail.com>
2023-12-05 10:32:19 -0500

    Update rotation stitching
    Changed name and functionality with primary view

--- RotationStitching.py
+++ RotationStitching.py
@@ -2,7 +2,7 @@ import open3d as o3d
 import numpy as np
 import os
 
-def stitch_rotating_object(directory, rotation_axis='y', degrees_per_capture=30):
+def stitch_pcds(directory, rotation_axis='y', degrees_per_capture=30):
     """
     Stitch together point clouds of an object rotating in front of a stationary camera.
     :param directory: Directory where the point cloud files are stored.
@@ -34,7 +34,7 @@ def stitch_rotating_object(directory, rotation_axis='y', degrees_per_capture=30)
     stitched_pc = o3d_point_clouds[0]
     for i in range(1, len(o3d_point_clouds)):
         # Define the rotation matrix
-        angle = np.radians(i*degrees_per_capture) # Makes view 0 Primary Viewing angle
+        angle = np.radians((i-1)*degrees_per_capture) # Makes view 1 Primary Viewing angle
         rotation_matrix = np.eye(4)
         rotation_matrix[:3, :3] = o3d.geometry.get_rotation_matrix_from_axis_angle(angle * np.eye(3)[:, axis_dict[rotation_axis]])
         # Apply rotation
@@ -51,7 +51,7 @@ def stitch_rotating_object(directory, rotation_axis='y', degrees_per_capture=30)
 # Example usage
 directory = "/path/to/pointcloud/files"
 file_prefix = "pointcloud"
-stitched_pc = stitch_rotating_object(directory, file_prefix, rotation_axis='z', degrees_per_capture=10)
+stitched_pc = stitch_pcds(directory, file_prefix, rotation_axis='z', degrees_per_capture=10)
 
 
 o3d.visualization.draw_geometries([stitched_pc])
